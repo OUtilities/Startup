bc2a8f267a46dff8733872f9eaa2b57e7eed365f
Oleg Lazarovych <anksayneyu@gmail.com>
2025-10-18 16:23:46 +0300

    add velvetscreen profile (#2)

---
+++ src/Quick.Startup.Utilities/O/Profiles/VelvetScreenProfile.cs
@@ -0,0 +1,27 @@
+﻿namespace O.Profiles;
+
+// VelvetScreen
+internal class VelvetScreenProfile : IProfile
+{
+    public Dictionary<string, string> GetRepoMappings()
+    {
+        return new()
+            {
+                { "utils", @"C:\_PetProjects\OUtilities\Startup" },
+                { "vui", @"C:\_PetProjects\VelvetScreen\angular-monolith-ui" },
+                { "vapi", @"C:\_PetProjects\VelvetScreen\angular-monolith-ui" },
+            };
+    }
+
+    public Dictionary<string, string> GetVsSolutionMappings()
+    {
+        return new()
+            {
+                { "startup", @"C:\_Olo\Quick.Startup.Utilities\Quick.Startup.Utilities.sln" },
+
+                { "vui", @"C:\_PetProjects\VelvetScreen\angular-monolith-ui" },
+                { "vapi", @"C:\_PetProjects\VelvetScreen\angular-monolith-ui" },
+            };
+    }
+}
+
